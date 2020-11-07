3077197bea78391549a175456437774f73339757
zungun <pi@raspberrypi>
2020-11-08 06:14:28 +0900

    server ok

--- server_test.py
+++ server_test.py
@@ -6,23 +6,32 @@ Usage::
 """
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import logging
+import json
 
 class S(BaseHTTPRequestHandler):
     def _set_response(self):
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
         self.end_headers()
-
+    """
     def do_GET(self):
         logging.info("GET request,\nPath: %s\nHeaders:\n%s\n", str(self.path), str(self.headers))
         self._set_response()
         self.wfile.write("GET request for {}".format(self.path).encode('utf-8'))
-
+    """
     def do_POST(self):
         content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
         post_data = self.rfile.read(content_length) # <--- Gets the data itself
         logging.info("POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n",
                 str(self.path), str(self.headers), post_data.decode('utf-8'))
+        json_object = json.loads(post_data.decode('utf-8'))
+        println(json_object['0'])
+        println(json_object['1'])  
+        println(json_object['2'])
+        println(json_object['3'])
+        println(json_object['4'])
+        println(json_object['5'])
+        println(json_object['6'])
         self._set_response()
         self.wfile.write("POST request for {}".format(self.path).encode('utf-8'))
 
